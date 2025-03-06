7e997657b1bfa5fc10b3d457120a09c5c9b4bbb5
FabioBatSilva <fabio.bat.silva@gmail.com>
2025-03-06 23:07:17 +0000

    Upgrade to phpunit 10

--- php/tests/autoload.php
+++ php/tests/autoload.php
@@ -1,24 +1,5 @@
 <?php
 
-/**
- * Custom error handler to suppress PHPUnit warnings in more recent version of php.
- *
- * @param int    $errno   The level of the error raised
- * @param string $errstr  The error message
- * @param string $errfile The filename that the error was raised in
- *
- * @return bool Returns true to suppress the error, false to let PHP handle it
- */
-set_error_handler(function ($errno, $errstr, $errfile) {
-    // If the error comes from a vendor file (e.g. from PHPUnit’s directory), ignore it.
-    if (strpos($errfile, '/vendor/phpunit/') !== false) {
-        return true; // signal that the error has been handled
-    }
-
-    // Otherwise, let PHP's normal error handling (or PHPUnit's handler) do its work.
-    return false;
-});
-
 $autoloadPath = __DIR__ . '/../vendor/autoload.php';
 
 if (!file_exists($autoloadPath)) {
--- php/tests/test_base.php
+++ php/tests/test_base.php
@@ -12,42 +12,6 @@ class TestBase extends \PHPUnit\Framework\TestCase
         TestUtil::setTestMessage($m);
     }
 
-    /**
-     * Polyfill for phpunit6.
-     */
-    static public function assertStringContains($needle, $haystack)
-    {
-        if (method_exists('\PHPUnit\Framework\TestCase', 'assertStringContainsString')) {
-            parent::assertStringContainsString($needle, $haystack);
-        } else {
-            parent::assertContains($needle, $haystack);
-        }
-    }
-
-    /**
-     * Polyfill for phpunit6.
-     */
-    static public function assertStringNotContains($needle, $haystack)
-    {
-        if (method_exists('\PHPUnit\Framework\TestCase', 'assertStringContainsString')) {
-            parent::assertStringNotContainsString($needle, $haystack);
-        } else {
-            parent::assertNotContains($needle, $haystack);
-        }
-    }
-
-    /**
-     * Polyfill for phpunit6.
-     */
-    static public function assertFloatEquals($expected, $actual, $delta)
-    {
-        if (method_exists('\PHPUnit\Framework\TestCase', 'assertEqualsWithDelta')) {
-            parent::assertEqualsWithDelta($expected, $actual, $delta);
-        } else {
-            parent::assertEquals($expected, $actual, '', $delta);
-        }
-    }
-
     public function setFields2(TestMessage $m)
     {
         TestUtil::setTestMessage2($m);
